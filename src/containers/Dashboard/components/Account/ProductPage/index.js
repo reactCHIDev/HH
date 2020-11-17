eedebb51bedac5e5984cc088d8d54b98a860f17c
YalovyyAlex <YalovyyAlex@gmail.com>
2020-11-17 12:56:48 +0200

    make product page adaptive

--- src/containers/Dashboard/components/Account/ProductPage/index.js
+++ src/containers/Dashboard/components/Account/ProductPage/index.js
@@ -14,21 +14,23 @@ import './product_page.less'
 const ProductPage = () => {
   return (
     <div className={styles.container}>
-      <div className={styles.content}>
-        <ImagePreviewer images={[stub1, stub2, stub3, stub4, stub1, stub2, stub3, stub4]} />
-        <div className={styles.inner_content}>
-          <Header text="Pie with carrots, apple and cinnamon" />
-          <Toolbar
-            price={12.59}
-            weightOptions={[250, 450, 600]}
-            isShowWeightOptions
-            isPreOrderOnly={false}
-          />
-          <Tabs />
-          <AboutMaker
-            name="Annette P."
-            text="I’m a nutritionist ,and baking cooking instructor. When I was younger I went to England for a year. The experience I had sharing a dormitory kitchen a nutritionist ,and baking cooking nutritionist ,and baking cooking instructor. When I was cooking."
-          />
+      <div className={styles.product}>
+        <div className={styles.content}>
+          <ImagePreviewer images={[stub1, stub2, stub3, stub4, stub1, stub2, stub3, stub4]} />
+          <div className={styles.inner_content}>
+            <Header text="Pie with carrots, apple and cinnamon" />
+            <Toolbar
+              price={12.59}
+              weightOptions={[250, 450, 600]}
+              isShowWeightOptions
+              isPreOrderOnly={false}
+            />
+            <Tabs />
+            <AboutMaker
+              name="Annette P."
+              text="I’m a nutritionist ,and baking cooking instructor. When I was younger I went to England for a year. The experience I had sharing a dormitory kitchen a nutritionist ,and baking cooking nutritionist ,and baking cooking instructor. When I was cooking."
+            />
+          </div>
         </div>
       </div>
     </div>
