dabe462c20c027b2e38bf43714b0e12a0e9d3472
Alexey Lazarev <al@bigdig.com.ua>
2020-11-19 16:46:45 +0200

    photo loader errors

--- src/containers/Dashboard/components/Account/AddProduct/components/Steps/Step3/index.js
+++ src/containers/Dashboard/components/Account/AddProduct/components/Steps/Step3/index.js
@@ -1,8 +1,8 @@
 import React, { useState, useEffect } from 'react'
 import T from 'prop-types'
-import { Button } from 'antd'
 import cls from 'classnames'
-import { Upload, Modal } from 'antd'
+import axios from 'axios'
+import { Button, Upload, Modal, Progress } from 'antd'
 import { PlusOutlined } from '@ant-design/icons'
 import { getItem, setItem } from 'utils/localStorage'
 import styles from './step3.module.scss'
@@ -19,12 +19,22 @@ function getBase64(file) {
 
 const Step3 = (props) => {
   const { setStep } = props
+  const [coverPhoto, setCoverPhoto] = useState([])
   const [otherPhotos, setOtherPhotos] = useState([])
-
   const [previewVisible, setPreviewVisible] = useState(false)
   const [previewImage, setPreviewImage] = useState('')
   const [previewTitle, setPreviewTitle] = useState('')
-  const [fileList, addFileList] = useState([])
+  const [defaultFileList, addFileList] = useState([])
+  const [url, setUrl] = useState('')
+  const [progress, setProgress] = useState(0)
+
+  useEffect(() => {
+    if (defaultFileList.length) {
+      const list = [...defaultFileList]
+      list[list.length - 1].url = url
+      addFileList(list)
+    }
+  }, [url])
 
   const handleCancel = () => setPreviewVisible(false)
 
@@ -38,15 +48,33 @@ const Step3 = (props) => {
     setPreviewTitle(file.name || file.url.substring(file.url.lastIndexOf('/') + 1))
   }
 
-  const handleChange = ({ fileList }) => addFileList(fileList)
+  const handleChange = ({ fileList }) => {
+    let list = []
+
+    if (defaultFileList.length < fileList.length) {
+      list = fileList.map((e, i) =>
+        i === fileList.length - 1
+          ? {
+              uid: e.uid,
+              name: e.name,
+              status: 'done',
+            }
+          : e,
+      )
+    } else {
+      list = defaultFileList.filter((e) => fileList.find((f) => f.uid === e.uid))
+    }
+    addFileList(list)
+  }
 
   const onNext = (data) => {
     const prevSteps = getItem('addProduct')
 
     const formData = {}
 
-    formData.coverPhoto = fileList.length ? fileList[0].response.url : ''
-    formData.otherPhotos = fileList.length ? fileList.slice(1).map((f) => f.response.url) : []
+    formData.coverPhoto = defaultFileList.length ? defaultFileList[0].url : ''
+    formData.otherPhotos =
+      defaultFileList.length > 1 ? defaultFileList.slice(1).map((f) => f.url) : []
 
     setItem('addProduct', {
       ...prevSteps,
@@ -62,8 +90,41 @@ const Step3 = (props) => {
     </div>
   )
 
-  const onFinish = (values) =>
-    console.log('%c   values   ', 'color: white; background: royalblue;', values)
+  async function sendFile(options) {
+    const { onSuccess, onError, file, onProgress } = options
+    const formData = new FormData()
+    formData.append('file', file)
+    const headers = {
+      'Content-Type': 'multipart/form-data',
+      Accept: 'application/json',
+      type: 'formData',
+      'x-api-key': '11edff01b8c5e3cfa0027fd313365f264b',
+      // Authorization:
+      // 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImFsQGJpZ2RpZy5jb20udWEiLCJwcm9maWxlTmFtZSI6IkFsZXhGTSIsInJvbGUiOiJGT09ETUFLRVIiLCJpYXQiOjE2MDUyNzU5Nzh9.QluuzPvYk3e4g_mMFD-mVvnWJknyl1OIxz3fAwuemzc',
+    }
+    try {
+      const res = await axios.post(
+        'https://hungryhugger.wildwebart.com/api/v1/file/upload/photo',
+        formData,
+        {
+          headers,
+          onUploadProgress: (event) => {
+            const percent = Math.floor((event.loaded / event.total) * 100)
+            setProgress(percent)
+            if (percent === 100) {
+              setTimeout(() => setProgress(0), 3000)
+            }
+            onProgress({ percent: (event.loaded / event.total) * 100 })
+          },
+        },
+      )
+      setUrl(res.data)
+    } catch (error) {
+      console.log('error', error)
+      // addFileList(defaultFileList.slice(0, -1))
+      // throw new Error(`ERROR`)
+    }
+  }
 
   return (
     <div className={styles.container}>
@@ -71,13 +132,13 @@ const Step3 = (props) => {
         <p className={styles.header}>Add cover & photos</p>
         <div className="photo_container">
           <Upload
-            action="https://www.mocky.io/v2/5cc8019d300000980a055e76"
+            customRequest={sendFile}
             listType="picture-card"
-            fileList={fileList}
+            fileList={defaultFileList}
             onPreview={handlePreview}
             onChange={handleChange}
           >
-            {fileList.length >= 10 ? null : uploadButton}
+            {defaultFileList.length >= 8 || progress > 0 ? null : uploadButton}
           </Upload>
           <Modal
             visible={previewVisible}
@@ -87,8 +148,15 @@ const Step3 = (props) => {
           >
             <img alt="example" style={{ width: '100%' }} src={previewImage} />
           </Modal>
+          {progress > 0 ? <Progress percent={progress} /> : <div style={{ height: 20 }} />}
         </div>
-        <Button type="primary" block disabled={fileList.length < 3} size="large" onClick={onNext}>
+        <Button
+          type="primary"
+          block
+          disabled={defaultFileList.length < 3}
+          size="large"
+          onClick={onNext}
+        >
           NEXT
         </Button>
       </div>
