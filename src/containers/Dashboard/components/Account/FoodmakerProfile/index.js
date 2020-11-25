5c83b0f7f52d5f9f8205e5224fe7fba612dac345
Alexey Lazarev <al@bigdig.com.ua>
2020-11-25 12:29:19 +0200

    profile markup

--- src/containers/Dashboard/components/Account/FoodmakerProfile/index.js
+++ src/containers/Dashboard/components/Account/FoodmakerProfile/index.js
@@ -1,10 +1,12 @@
 import React, { useState, useEffect } from 'react'
 import T from 'prop-types'
 import { getUserAccount, updateFoodmakerAccountAC } from 'actions/account'
-import { Upload, Progress } from 'antd'
+import { Upload, Modal, Progress } from 'antd'
 import ImgCrop from 'antd-img-crop'
 import Button from 'components/Button'
 import { Input, Select, InputNumber, Checkbox } from 'antd'
+import { PlusOutlined } from '@ant-design/icons'
+
 import EditIcon from 'assets/icons/svg/editor-icon.svg'
 import { connect } from 'react-redux'
 import { getItem } from 'utils/localStorage'
@@ -12,16 +14,33 @@ import axios from 'axios'
 import styles from './foodmakerprofile.module.scss'
 import './foodmakerprofile.less'
 
+function getBase64(file) {
+  return new Promise((resolve, reject) => {
+    const reader = new FileReader()
+    reader.readAsDataURL(file)
+    reader.onload = () => resolve(reader.result)
+    reader.onerror = (error) => reject(error)
+  })
+}
+
 const FoodmakerProfile = (props) => {
   const { account, getUserAccount, updateFoodmakerAccountAC } = props
   const { success } = account
 
   const [defaultFileList, setFileList] = useState([])
   const [url, setUrl] = useState('')
+  const [galleryFileList, setGalleryFileList] = useState([])
+  const [galleryUrl, setGalleryUrl] = useState('')
+
+  const [previewVisible, setPreviewVisible] = useState(false)
+  const [previewImage, setPreviewImage] = useState('')
+  const [previewTitle, setPreviewTitle] = useState('')
+
   const [progress, setProgress] = useState(0)
   const [firstName, setFirstName] = useState('')
   const [lastName, setLastName] = useState('')
   const [selectedItems, setSelectedItems] = useState([])
+  const [selectedLangs, setSelectedLangs] = useState([])
 
   const tags = [
     { id: 1, tagName: 'Drink' },
@@ -31,12 +50,23 @@ const FoodmakerProfile = (props) => {
     { id: 5, tagName: 'Pasta' },
   ]
 
+  const langs = [
+    { id: 1, tagName: 'Japanese' },
+    { id: 2, tagName: 'English' },
+    { id: 3, tagName: 'French' },
+    { id: 4, tagName: 'Spanish' },
+    { id: 5, tagName: 'Chinese' },
+  ]
+
   const handleChangeTags = (selectedItms) => {
-    console.log('%c     selectedItms ', 'color: darkgreen; background: palegreen;', selectedItms)
     setSelectedItems(selectedItms)
+  }
+  const handleChangeLangs = (selectedLngs) => {
+    setSelectedLangs(selectedLngs)
   }
 
   const filteredTags = tags.filter((o) => !selectedItems.includes(o.id))
+  const filteredLangs = langs.filter((o) => !selectedLangs.includes(o.id))
 
   const { Option } = Select
 
@@ -66,10 +96,31 @@ const FoodmakerProfile = (props) => {
     }
   }, [url])
 
-  const onChange = ({ fileList: newFileList }) => {
-    let list = []
+  useEffect(() => {
+    if (galleryFileList.length) {
+      const list = [...galleryFileList]
+      list[list.length - 1].url = galleryUrl
+      setGalleryFileList(list)
+    }
+  }, [galleryUrl])
 
-    if (defaultFileList.length < newFileList.length) {
+  const handleCancel = () => setPreviewVisible(false)
+
+  const handlePreview = async (file) => {
+    if (!file.url && !file.preview) {
+      file.preview = await getBase64(file.originFileObj)
+    }
+
+    setPreviewImage(file.url || file.preview)
+    setPreviewVisible(true)
+    setPreviewTitle(file.name || file.url.substring(file.url.lastIndexOf('/') + 1))
+  }
+
+  const onChange = ({ fileList: newFileList }, avatar) => {
+    let list = []
+    const targetList = avatar ? defaultFileList : galleryFileList
+
+    if (targetList.length < newFileList.length) {
       list = newFileList.map((e, i) =>
         i === newFileList.length - 1
           ? {
@@ -80,9 +131,9 @@ const FoodmakerProfile = (props) => {
           : e,
       )
     } else {
-      list = defaultFileList.filter((e) => newFileList.find((f) => f.uid === e.uid))
+      list = targetList.filter((e) => newFileList.find((f) => f.uid === e.uid))
     }
-    setFileList(list)
+    avatar ? setFileList(list) : setGalleryFileList(list)
   }
 
   const onPreview = async (file) => {
@@ -100,7 +151,7 @@ const FoodmakerProfile = (props) => {
     imgWindow.document.write(image.outerHTML)
   }
 
-  async function sendFile(options) {
+  async function sendFile(options, avatar) {
     const { onSuccess, onError, file, onProgress } = options
     const formData = new FormData()
     formData.append('file', file)
@@ -128,7 +179,7 @@ const FoodmakerProfile = (props) => {
           },
         },
       )
-      setUrl(res.data)
+      avatar ? setUrl(res.data) : setGalleryUrl(res.data)
     } catch (error) {
       console.log('error', error)
       // addFileList(defaultFileList.slice(0, -1))
@@ -138,7 +189,7 @@ const FoodmakerProfile = (props) => {
 
   const onChangeName = (e) => setFirstName(e.target.value)
   const onChangeLastName = (e) => setLastName(e.target.value)
-  const onChangeUrl = (e) => setUrl(e.target.value)
+  const onChangeUrl = (e) => {} // setUrl(e.target.value)
 
   const onSubmit = () => {
     const payload = {}
@@ -149,10 +200,18 @@ const FoodmakerProfile = (props) => {
     // updateFoodmakerAccountAC(payload)
   }
 
+  const uploadButton = (
+    <div>
+      <PlusOutlined />
+      <div style={{ marginTop: 8 }}>Upload</div>
+    </div>
+  )
+
   return (
     <div className={styles.container}>
       <div className={styles.content}>
         <p className={styles.head}>Foodmaker Profile</p>
+
         <div id="uploader_fm" className={styles.uploader}>
           {success && <div className={styles.success}>Saved successfully</div>}
           {defaultFileList && (
@@ -160,10 +219,10 @@ const FoodmakerProfile = (props) => {
               <ImgCrop rotate>
                 <Upload
                   // action="https://www.mocky.io/v2/5cc8019d300000980a055e76"
-                  customRequest={sendFile}
+                  customRequest={(options) => sendFile(options, true)}
                   listType="picture-card"
                   fileList={defaultFileList}
-                  onChange={onChange}
+                  onChange={(options) => onChange(options, true)}
                   onPreview={onPreview}
                 >
                   {defaultFileList.length < 1 && '+ Upload'}
@@ -182,59 +241,109 @@ const FoodmakerProfile = (props) => {
           </div>
         </div>
 
-        <div className={styles.profile_name}>
-          <p className={styles.sec1}>Profile name</p>
+        <div className={styles.data_section}>
+          <div className={styles.profile_section}>
+            <p className={styles.sec1}>Profile name</p>
 
-          <div className={styles.user_data}>
-            <div className={styles.refer}>
-              <label className={styles.label}>Ms/Mss/Mr</label>
-              <Select onChange={() => {}}>
-                {discnt.map((n) => (
-                  <Option key={n.value} value={n.value}>
-                    {n.title}
-                  </Option>
+            <div className={styles.user_data}>
+              <div className={styles.refer}>
+                <label className={styles.label}>Ms/Mss/Mr</label>
+                <Select onChange={() => {}}>
+                  {discnt.map((n) => (
+                    <Option key={n.value} value={n.value}>
+                      {n.title}
+                    </Option>
+                  ))}
+                </Select>
+              </div>
+              <div className={styles.first_name}>
+                <label className={styles.label}>First name</label>
+                <Input onChange={onChangeName} value={firstName} />
+              </div>
+              <div className={styles.last_name}>
+                <label className={styles.label}>Last name</label>
+                <Input onChange={onChangeLastName} value={lastName} />
+              </div>
+            </div>
+            <p className={styles.sec1}>About you</p>
+
+            <div className={styles.about}>
+              <Input.TextArea rows={4} />
+            </div>
+            <p className={styles.sec1}>Tags (Up to 5 tags for your speciality and services)</p>
+
+            <div className={styles.service_tags}>
+              <Select
+                mode="multiple"
+                value={selectedItems}
+                onChange={handleChangeTags}
+                showArrow
+                style={{ width: '100%' }}
+              >
+                {filteredTags.map((item) => (
+                  <Select.Option key={item.id} value={item.tagName}>
+                    {item.tagName}
+                  </Select.Option>
                 ))}
               </Select>
             </div>
-            <div className={styles.first_name}>
-              <label className={styles.label}>First name</label>
-              <Input onChange={onChangeName} value={firstName} />
-            </div>
-            <div className={styles.last_name}>
-              <label className={styles.label}>Last name</label>
-              <Input onChange={onChangeLastName} value={lastName} />
-            </div>
           </div>
-          <div className={styles.about}>
-            <label className={styles.label}>About you</label>
-            <Input.TextArea rows={4} />
-          </div>
-          <div className={styles.service_tags}>
-            <label className={styles.label}>
-              Tags (Up to 5 tags for your speciality and services)
-            </label>
-            <Select
-              mode="multiple"
-              value={selectedItems}
-              onChange={handleChangeTags}
-              showArrow
-              style={{ width: '100%' }}
-            >
-              {filteredTags.map((item) => (
-                <Select.Option key={item.id} value={item.tagName}>
-                  {item.tagName}
-                </Select.Option>
-              ))}
-            </Select>
-          </div>
-        </div>
 
-        <div className={styles.profile_url}>
-          <p className={styles.sec2}>Profile url</p>
-          <div className={styles.profile_data}>
-            <div className={styles.url_name}>
-              <label className={styles.label}>Try to make the link to your profile memorable</label>
-              <Input onChange={onChangeUrl} value={url} />
+          <div className={styles.gallery_section}>
+            <p className={styles.sec2}>Profile url</p>
+            <div className={styles.profile_url}>
+              <div className={styles.profile_data}>
+                <div className={styles.url_name}>
+                  <label className={styles.label}>
+                    Try to make the link to your profile memorable
+                  </label>
+                  <Input onChange={onChangeUrl} value={url} />
+                </div>
+              </div>
+            </div>
+
+            <div className={styles.gallery_container}>
+              <p className={styles.sec1}>Gallery</p>
+
+              <div className="photo_container">
+                <Upload
+                  // action="https://www.mocky.io/v2/5cc8019d300000980a055e76"
+                  // action="https://hungryhugger.wildwebart.com/api/v1/file/upload/photo"
+                  customRequest={sendFile}
+                  listType="picture-card"
+                  fileList={galleryFileList}
+                  onPreview={handlePreview}
+                  onChange={onChange}
+                >
+                  {defaultFileList.length >= 8 || progress > 0 ? null : uploadButton}
+                </Upload>
+                <Modal
+                  visible={previewVisible}
+                  title={previewTitle}
+                  footer={null}
+                  onCancel={handleCancel}
+                >
+                  <img alt="example" style={{ width: '100%' }} src={previewImage} />
+                </Modal>
+                {progress > 0 ? <Progress percent={progress} /> : <div style={{ height: 20 }} />}
+              </div>
+            </div>
+            <p className={styles.sec1}>Languages you speak</p>
+
+            <div className={styles.service_tags}>
+              <Select
+                mode="multiple"
+                value={selectedLangs}
+                onChange={handleChangeLangs}
+                showArrow
+                style={{ width: '100%' }}
+              >
+                {filteredLangs.map((item) => (
+                  <Select.Option key={item.id} value={item.tagName}>
+                    {item.tagName}
+                  </Select.Option>
+                ))}
+              </Select>
             </div>
           </div>
         </div>
