7f46dbb53474899c5080ccfcca46e50e84ed818a
Sasha Beloberdin <ab@bigdig.com.ua>
2021-02-16 12:53:40 +0200

    feature/experiences, fix review markup

--- src/containers/ExperienceDashboard/components/Reviews/components/Review/index.js
+++ src/containers/ExperienceDashboard/components/Reviews/components/Review/index.js
@@ -1,4 +1,6 @@
 import React from 'react'
+import { Rate } from 'antd'
+import people from 'assets/icons/svg/people.svg'
 import styles from './review.module.scss'
 
 function Review() {
@@ -20,10 +22,25 @@ function Review() {
           <div className={styles.waitingText}>Waiting for reply!</div>
         </div>
         <div className={styles.reviewInfo}>
-          <div className={styles.reviewMarks}>1</div>
-          <div className={styles.reviewDate}>1</div>
+          <div className={styles.reviewMarks}>
+            <div className={styles.rateWrapper}>
+              <Rate style={{ color: '#31394C' }} disabled value={4} />
+            </div>
+            <div className={styles.reads}>
+              <div className={styles.reads_people}>
+                <img src={people} alt="avatar" />
+              </div>
+              <div className={styles.qty_reads}>4</div>
+            </div>
+          </div>
+          <div className={styles.reviewDate}>22 May</div>
         </div>
-        <div className={styles.reviewText}>ads</div>
+        <div className={styles.reviewText}>
+          Super cool gin program they have going here. All the different infusions are super
+          interesting. Сocktails, was a great time. Super cool gin program they have going here. All
+          the different infusions are super interesting. Got to make some cocktails, was a great
+          time.
+        </div>
         <div className={styles.replyButton}>REPLY</div>
       </div>
     </div>
