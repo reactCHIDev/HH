e42d7fa238c35f4efb541415708567114b7cb22f
Sasha Beloberdin <ab@bigdig.com.ua>
2021-02-24 23:16:36 +0200

    feature/exp-page, add about section

--- src/pages/ExperiencePage/components/About/index.js
+++ src/pages/ExperiencePage/components/About/index.js
@@ -4,10 +4,32 @@ import styles from './about.module.scss'
 function About() {
   return (
     <div className={styles.container}>
-      <div>What we’ll do</div>
-      <div>What else you should know</div>
-      <div>What I’ll provide</div>
-      <div>Where will we meet</div>
+      <div className={styles.section}>
+        <h4>What we’ll do</h4>
+        <p>
+          You will taste the two Gins from Tanglin Distillery, Singapore’s first Gin Distillery. We
+          will explore the botanicals that go into the Gin and the process of making the Gin. And
+          using a Gin of your choice, we will teach you to make a bespoke cocktail. We will explore
+          the botanicals that go into the Gin and the process of making the Gin. You will taste the
+          two Gins from Tanglin Distillery, Singapore’s first Gin Distillery. And using a Gin of
+          your choice, we will teach you to make a bespoke cocktail.
+        </p>
+      </div>
+      <div className={styles.section}>
+        <h4>What I’ll provide</h4>
+        <p>
+          Snacks You will get to sample snacks that are unique to a culture like Savoury Plantain
+          Chips cooked in Coconut Oil from South India. Cocktail and Spirits You will get two
+          tasting portions of Gin. And a full cocktail made with Gin.
+        </p>
+      </div>
+      <div className={styles.section}>
+        <h4>Additional requirement</h4>
+        <p>
+          You need to be 18 and above to attend this session. You will consume about 90ml of booze
+          which is about three standard drinks.
+        </p>
+      </div>
     </div>
   )
 }
