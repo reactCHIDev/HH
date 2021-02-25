3de412085ae4120ceb1a9cafe8fabdeb465b1bde
Alexey Lazarev <al@bigdig.com.ua>
2021-02-25 21:50:42 +0200

    booking flow

--- src/pages/ExperiencePage/components/ExpHeader/index.js
+++ src/pages/ExperiencePage/components/ExpHeader/index.js
@@ -1,7 +1,9 @@
-import React, { useState, useEffect, useCallback, useMemo } from 'react'
+import React, { useState, useEffect } from 'react'
 import T from 'prop-types'
 import cls from 'classnames'
-import { Select, Slider, InputNumber, Checkbox, Button } from 'antd'
+import { parseISO, isSameDay } from 'date-fns'
+import { InputNumber, Button } from 'antd'
+import GuestsSelector from '../GuestsSelector'
 import { useForm, Controller } from 'react-hook-form'
 import _ from 'lodash/fp'
 import ImagePreviewer from 'pages/ProductPage/components/ImagePreviewer'
@@ -9,62 +11,143 @@ import Calendar from '../Calendar'
 import styles from './exp_page_header.module.scss'
 import './exp_page_header.less'
 
-const ExpHeader = () => {
-  const { register, handleSubmit, control, setValue, errors } = useForm({
+const ExpHeader = ({ experience, user }) => {
+  const { id, coverPhoto, otherPhotos, guests, discount, time, priceAdult, priceChild } = experience
+  const { firstName, lastName } = user
+
+  const [selectedDate, setSelectedDate] = useState()
+  const [selectedTime, setSelectedTime] = useState()
+  const [dates, setDates] = useState([])
+  const [appointments, setAppointments] = useState([])
+  const [visible, setVisibilityGuestsSelector] = useState(false)
+  const [adult, setAdultCount] = useState(1)
+  const [children, setChildrenCount] = useState(0)
+  const [total, setTotal] = useState(0)
+
+  const { handleSubmit, control, errors } = useForm({
     mode: 'onBlur',
   })
-  const qty = 2
-  const price = 46
-  const discount = 10
+
+  const onBook = (data) => {
+    const bookData = {
+      guests: {
+        adults: 10,
+        children: 5,
+      },
+      time: selectedTime,
+      experienceId: id,
+      totalPrice: 250,
+      currency: 'HKD',
+      firstName,
+      lastName,
+      phone: '123 12345678',
+    }
+    console.log('%c   bookData   ', 'color: darkgreen; background: palegreen;', bookData)
+  }
+
+  useEffect(() => {
+    setTotal(
+      Number(
+        (adult * priceAdult + children * priceChild) *
+          (1 - (adult + children >= discount.quantity ? discount.discount / 100 : 0)),
+      ).toFixed(2),
+    )
+  }, [adult, children])
+
+  useEffect(() => {
+    const getDates = (allDays) => {
+      if (!allDays) return
+      return allDays.reduce(
+        (acc, el, i, arr) =>
+          acc.find((d) => isSameDay(parseISO(el), parseISO(d))) ? acc : acc.concat([el]),
+        [],
+      )
+    }
+    if (time?.length) setDates(getDates(time))
+  }, [time])
+
+  useEffect(() => {
+    const getAppointments = (day, allDays) => {
+      if (!allDays) return
+      const thisDay = allDays.filter((d) => isSameDay(parseISO(day), parseISO(d)))
+      return thisDay
+    }
+    setAppointments(getAppointments(selectedDate, time))
+  }, [selectedDate])
 
   return (
-    <div className={styles.container}>
-      <div className={styles.content}>
-        <ImagePreviewer
-          images={[
-            'https://hungryhugger-space.fra1.digitaloceanspaces.com/72cb3de3-61ef-4149-afae-75f9bf1be702_1611842017824.jpg',
-            'https://hungryhugger-space.fra1.digitaloceanspaces.com/72cb3de3-61ef-4149-afae-75f9bf1be702_1611842017824.jpg',
-            'https://hungryhugger-space.fra1.digitaloceanspaces.com/72cb3de3-61ef-4149-afae-75f9bf1be702_1611842017824.jpg',
-            'https://hungryhugger-space.fra1.digitaloceanspaces.com/72cb3de3-61ef-4149-afae-75f9bf1be702_1611842017824.jpg',
-            'https://hungryhugger-space.fra1.digitaloceanspaces.com/72cb3de3-61ef-4149-afae-75f9bf1be702_1611842017824.jpg',
-            'https://hungryhugger-space.fra1.digitaloceanspaces.com/72cb3de3-61ef-4149-afae-75f9bf1be702_1611842017824.jpg',
-          ]}
-        />
-        <div className={styles.inner_content}>
-          <p className={styles.exp_heading}>Experience title</p>
-          <div className={cls(styles.input_number, 'exp-guests_number')}>
-            <label className={styles.label}>Number of guests</label>
-            <Controller
-              control={control}
-              name="guests"
-              rules={{ required: true }}
-              render={({ onChange, value, name }) => (
+    <div className={styles.wrapper}>
+      <div className={cls(styles.container, styles.gray)}>
+        <div className={styles.content}>
+          <ImagePreviewer images={[coverPhoto].concat(otherPhotos)} />
+          <div className={styles.inner_content}>
+            <p className={styles.exp_heading}>Experience title</p>
+            <form className={styles.form} onSubmit={handleSubmit(onBook)}>
+              <div
+                className={cls(styles.input_number, 'exp-guests_number')}
+                onClick={() => setVisibilityGuestsSelector((v) => !v)}
+              >
+                <GuestsSelector
+                  visible={visible}
+                  discount={discount}
+                  guests={guests}
+                  priceAdult={priceAdult}
+                  priceChild={priceChild}
+                  adult={adult}
+                  setAdultCount={setAdultCount}
+                  children={children}
+                  setChildrenCount={setChildrenCount}
+                />
+
+                <label className={styles.label}>Number of guests</label>
                 <div className={cls(styles.input_wrapper, 'exp-page-input')}>
-                  <div className={styles.success}>10% OFF</div>
-                  <InputNumber name={name} value={value} onChange={onChange} />
+                  <div className={styles.success}>
+                    {adult + children >= discount.quantity ? `${discount.discount}% OFF` : ''}
+                  </div>
+                  <input
+                    className={styles.input}
+                    type="text"
+                    value={`${adult + children} people`}
+                  />
                 </div>
-              )}
-            />
-            {_.get('guests.type', errors) === 'required' && (
-              <p className={styles.errmsg}>This field is required</p>
-            )}
-          </div>
-          <Calendar />
-          <div className={styles.info_wrapper}>
-            <div className={styles.info_container}>
-              <div className={styles.info_text}>{`Adults x ${qty} ($${price}) = $${qty *
-                price}`}</div>
-              <div className={styles.info_text}>{`Children x ${qty} ($${price}) = $${qty *
-                price}`}</div>
-              <div className={styles.info_text}>
-                Total <span>{` (with ${discount}% discount) `}</span> = 1100HKD
               </div>
-            </div>
-            <div className={cls(styles.button_container, 'booking_btn')}>
-              <Button type="primary" block size="large">
-                BOOK
-              </Button>
-            </div>
+              <Calendar
+                selectedDate={selectedDate}
+                setSelectedDate={setSelectedDate}
+                selectedTime={selectedTime}
+                setSelectedTime={setSelectedTime}
+                dates={dates}
+                appointments={appointments}
+              />
+              <div className={styles.info_wrapper}>
+                <div className={styles.info_container}>
+                  <div className={styles.info_text}>
+                    {`Adults x ${adult} ($${priceAdult}) = $${adult * priceAdult}`}
+                  </div>
+                  <div
+                    className={styles.info_text}
+                  >{`Children x ${children} ($${priceChild}) = $${children * priceChild}`}</div>
+                  <div className={styles.info_text}>
+                    Total
+                    <span
+                      className={
+                        adult + children >= discount.quantity
+                          ? styles.discount_total
+                          : styles.no_discount
+                      }
+                    >
+                      {` (with ${discount.discount}% discount) `}
+                    </span>
+                    {` = ${total} HKD`}
+                  </div>
+                </div>
+                <div className={cls(styles.button_container, 'booking_btn')}>
+                  <Button type="primary" block size="large" htmlType="submit">
+                    {`BOOK FOR ${total} HKD`}
+                  </Button>
+                </div>
+              </div>
+            </form>
           </div>
         </div>
       </div>
