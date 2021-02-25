98d22503dddf0ab446c2f3f6072664117aa3be16
Alexey Lazarev <al@bigdig.com.ua>
2021-02-25 13:34:38 +0200

    fixes exp page

--- src/pages/ExperiencePage/components/ExpHeader/index.js
+++ src/pages/ExperiencePage/components/ExpHeader/index.js
@@ -18,51 +18,53 @@ const ExpHeader = () => {
   const discount = 10
 
   return (
-    <div className={styles.content}>
-      <ImagePreviewer
-        images={[
-          'https://hungryhugger-space.fra1.digitaloceanspaces.com/72cb3de3-61ef-4149-afae-75f9bf1be702_1611842017824.jpg',
-          'https://hungryhugger-space.fra1.digitaloceanspaces.com/72cb3de3-61ef-4149-afae-75f9bf1be702_1611842017824.jpg',
-          'https://hungryhugger-space.fra1.digitaloceanspaces.com/72cb3de3-61ef-4149-afae-75f9bf1be702_1611842017824.jpg',
-          'https://hungryhugger-space.fra1.digitaloceanspaces.com/72cb3de3-61ef-4149-afae-75f9bf1be702_1611842017824.jpg',
-          'https://hungryhugger-space.fra1.digitaloceanspaces.com/72cb3de3-61ef-4149-afae-75f9bf1be702_1611842017824.jpg',
-          'https://hungryhugger-space.fra1.digitaloceanspaces.com/72cb3de3-61ef-4149-afae-75f9bf1be702_1611842017824.jpg',
-        ]}
-      />
-      <div className={styles.inner_content}>
-        <p className={styles.exp_heading}>Experience title</p>
-        <div className={cls(styles.input_number, 'exp-guests_number')}>
-          <label className={styles.label}>Number of guests</label>
-          <Controller
-            control={control}
-            name="guests"
-            rules={{ required: true }}
-            render={({ onChange, value, name }) => (
-              <div className={cls(styles.input_wrapper, 'exp-page-input')}>
-                <div className={styles.success}>10% OFF</div>
-                <InputNumber name={name} value={value} onChange={onChange} />
-              </div>
+    <div className={styles.container}>
+      <div className={styles.content}>
+        <ImagePreviewer
+          images={[
+            'https://hungryhugger-space.fra1.digitaloceanspaces.com/72cb3de3-61ef-4149-afae-75f9bf1be702_1611842017824.jpg',
+            'https://hungryhugger-space.fra1.digitaloceanspaces.com/72cb3de3-61ef-4149-afae-75f9bf1be702_1611842017824.jpg',
+            'https://hungryhugger-space.fra1.digitaloceanspaces.com/72cb3de3-61ef-4149-afae-75f9bf1be702_1611842017824.jpg',
+            'https://hungryhugger-space.fra1.digitaloceanspaces.com/72cb3de3-61ef-4149-afae-75f9bf1be702_1611842017824.jpg',
+            'https://hungryhugger-space.fra1.digitaloceanspaces.com/72cb3de3-61ef-4149-afae-75f9bf1be702_1611842017824.jpg',
+            'https://hungryhugger-space.fra1.digitaloceanspaces.com/72cb3de3-61ef-4149-afae-75f9bf1be702_1611842017824.jpg',
+          ]}
+        />
+        <div className={styles.inner_content}>
+          <p className={styles.exp_heading}>Experience title</p>
+          <div className={cls(styles.input_number, 'exp-guests_number')}>
+            <label className={styles.label}>Number of guests</label>
+            <Controller
+              control={control}
+              name="guests"
+              rules={{ required: true }}
+              render={({ onChange, value, name }) => (
+                <div className={cls(styles.input_wrapper, 'exp-page-input')}>
+                  <div className={styles.success}>10% OFF</div>
+                  <InputNumber name={name} value={value} onChange={onChange} />
+                </div>
+              )}
+            />
+            {_.get('guests.type', errors) === 'required' && (
+              <p className={styles.errmsg}>This field is required</p>
             )}
-          />
-          {_.get('guests.type', errors) === 'required' && (
-            <p className={styles.errmsg}>This field is required</p>
-          )}
-        </div>
-        <Calendar />
-        <div className={styles.info_wrapper}>
-          <div className={styles.info_container}>
-            <div className={styles.info_text}>{`Adults x ${qty} ($${price}) = $${qty *
-              price}`}</div>
-            <div className={styles.info_text}>{`Children x ${qty} ($${price}) = $${qty *
-              price}`}</div>
-            <div className={styles.info_text}>
-              Total <span>{` (with ${discount}% discount) `}</span> = 1100HKD
-            </div>
           </div>
-          <div className={cls(styles.button_container, 'booking_btn')}>
-            <Button type="primary" block size="large">
-              BOOK
-            </Button>
+          <Calendar />
+          <div className={styles.info_wrapper}>
+            <div className={styles.info_container}>
+              <div className={styles.info_text}>{`Adults x ${qty} ($${price}) = $${qty *
+                price}`}</div>
+              <div className={styles.info_text}>{`Children x ${qty} ($${price}) = $${qty *
+                price}`}</div>
+              <div className={styles.info_text}>
+                Total <span>{` (with ${discount}% discount) `}</span> = 1100HKD
+              </div>
+            </div>
+            <div className={cls(styles.button_container, 'booking_btn')}>
+              <Button type="primary" block size="large">
+                BOOK
+              </Button>
+            </div>
           </div>
         </div>
       </div>
--- src/pages/ExperiencePage/components/MainInfo/ImageWrapper/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import styles from './imageWrapper.module.scss'
-
-function ImageWrapper() {
-  return <div className={styles.container}>image</div>
-}
-
-export default ImageWrapper
