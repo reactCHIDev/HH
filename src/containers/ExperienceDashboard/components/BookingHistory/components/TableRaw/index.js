4bf5e12afb30c994f303ac76a9a5c554ada05fc2
Sasha Beloberdin <ab@bigdig.com.ua>
2021-02-28 22:52:02 +0200

    feature/experiences, add booking history

---
+++ src/containers/ExperienceDashboard/components/BookingHistory/components/TableRaw/index.js
@@ -0,0 +1,36 @@
+/* eslint-disable react/prop-types */
+import React from 'react'
+import styles from './tableRaw.module.scss'
+
+function TableRaw({ element }) {
+  const day = new Date(element.time).toLocaleDateString('en-US', {
+    month: 'short',
+    day: 'numeric',
+    year: '2-digit',
+  })
+
+  const time = new Date(element.time).toLocaleTimeString('en-US', {
+    hour: 'numeric',
+    hour12: false,
+    minute: '2-digit',
+  })
+  return (
+    <div className={styles.container}>
+      <div className={styles.mainInfo}>
+        <div style={{ backgroundImage: `url("${element.photo}")` }} className={styles.imgWrapper} />
+        <div>{element.title}</div>
+      </div>
+      <div className={styles.secondaryInfo}>
+        <div className={styles.day}>{day}</div>
+        <div className={styles.time}>{time}</div>
+        <div className={styles.price}>{element.price}$</div>
+        <div className={styles.guests}>{element.guests}</div>
+        <div className={styles.button}>
+          <button type="button">view</button>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default TableRaw
--- src/sagas/bookings_history.js
+++ src/sagas/bookings_history.js
@@ -10,7 +10,19 @@ import {
 function* getFMBookingHistorySaga() {
   try {
     const response = yield getFMBookingHistory()
-    yield put({ type: GET_FM_BOOKING_HISTORY_SUCCESS, data: response.data.bookings })
+    yield put({
+      type: GET_FM_BOOKING_HISTORY_SUCCESS,
+      data: response.data.bookings.map(({ ...data }) => ({
+        adults: data.guests.adults || 0,
+        childs: data.guests.childs || 0,
+        id: data.id,
+        title: data.experience.title,
+        photo: data.experience.coverPhoto,
+        time: data.time,
+        price: data.totalPrice,
+        guests: data.guests.adults || 0 + data.guests.childs || 0,
+      })),
+    })
   } catch (error) {
     if (error.response) {
       yield put({ type: GET_FM_BOOKING_HISTORY_ERROR, error: error.response.data.error })
