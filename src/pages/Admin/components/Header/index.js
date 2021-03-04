b7ed145ed8b7e53067bc60d9c21fa9dda27c5faf
Alexey Lazarev <al@bigdig.com.ua>
2021-03-04 22:54:11 +0200

    minor fixes

--- src/pages/Admin/components/Header/index.js
+++ src/pages/Admin/components/Header/index.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Layout, Menu } from 'antd'
 import LogoWhite from 'assets/images/logo_nobeta.svg'
+import { Link } from 'react-router-dom'
 
 import styles from './header.module.scss'
 
@@ -10,10 +11,16 @@ function HeaderWrapper() {
   return (
     <Header>
       <div className={styles.logo}>
-        <img alt="logo" src={LogoWhite} />
+        <Link className={styles.link} to="/">
+          <img alt="logo" src={LogoWhite} />
+        </Link>
       </div>
       <Menu theme="dark" mode="horizontal">
-        <Menu.Item key="1">Home</Menu.Item>
+        <Menu.Item key="1">
+          <Link className={styles.link} to="/">
+            Home
+          </Link>
+        </Menu.Item>
         <Menu.Item key="2">Account</Menu.Item>
         <Menu.Item key="3">Exit</Menu.Item>
       </Menu>
